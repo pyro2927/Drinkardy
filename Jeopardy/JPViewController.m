6dc9f200742b010ddfc265e2a4ba65a15cc87f05
pyro2927 <joseph@pintozzi.com>
2013-01-28 08:00:24 -0600

    Initial commit. Just showing categories at the top

--- Jeopardy/JPViewController.m
+++ Jeopardy/JPViewController.m
@@ -17,7 +17,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+//    create our array of category strings
+    NSArray *categories = @[@"Sound Bytes", @"It's a Me-ah", @"Trusty Companions", @"Power Ups", @"Multiplayer Mayhem", @"The Final Boss"];
+//    calculate the screen width divided by the number of categories we have
+    CGFloat categoryWidth = 1024 / [categories count];
+//    iterate over our array
+    for (int i = 0; i < [categories count]; i++) {
+//        create a new label
+        UILabel *categoryLabel = [[UILabel alloc] initWithFrame:CGRectMake(i * categoryWidth, 0, categoryWidth, 100)];
+//        set our category text to the label
+        [categoryLabel setText:[categories objectAtIndex:i]];
+//        have our text center align
+        [categoryLabel setTextAlignment:NSTextAlignmentCenter];
+//        add that label to our view
+        [self.view addSubview:categoryLabel];
+    }
 }
 
 - (void)didReceiveMemoryWarning
